21599a7b1e6d1580d6c817066fa7532a77489530
Larksel <lemuelgames@outlook.com>
2024-10-30 22:47:34 -0300

    :sparkles: feat: Header criado com botões de acessibilidade

--- SeniorCareManagerFrontend/src/App.tsx
+++ SeniorCareManagerFrontend/src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Layout from "./components/Layout";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <Layout />
+  );
 }
-
-export default App
---
+++ SeniorCareManagerFrontend/src/components/Layout/index.tsx
@@ -0,0 +1,9 @@
+import Header from "../Header";
+
+export default function Layout() {
+  return (
+    <div className='flex flex-col text-text bg-background min-h-screen'>
+      <Header />
+    </div>
+  );
+}
