f6cc8578b7cbe67535afe037366fa82d9a90157e
Larksel <lemuelgames@outlook.com>
2024-10-30 23:06:30 -0300

    :sparkles: feat: Rodapé criado com logotipo da fatec jales e do CPS

--- SeniorCareManagerFrontend/src/components/Layout/index.tsx
+++ SeniorCareManagerFrontend/src/components/Layout/index.tsx
@@ -1,9 +1,14 @@
 import Header from "../Header";
+import Footer from "../Footer";
 
 export default function Layout() {
   return (
     <div className='flex flex-col text-text bg-background min-h-screen'>
       <Header />
+
+      {/* Conteúdo */}
+
+      <Footer />
     </div>
   );
 }
