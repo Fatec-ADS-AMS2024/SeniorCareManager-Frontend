af019faa04bb6769759632bde78cb44ec15df8a7
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-04 16:27:50 -0300

    :lipstick: feat: Texto adicionado na página inicial

---
+++ SeniorCareManagerFrontend/src/pages/LandingPage/index.tsx
@@ -0,0 +1,20 @@
+export default function LandingPage() {
+  return (
+    <div className="">
+      <div className="w-full bg-surface flex flex-col ">
+        <h1 className="text-secondary font-bold text-4xl mx-16 mt-28">
+          Plataforma de ferramentas para gerenciar o cuidado e bem-estar de
+          idosos.
+        </h1>
+        <p className="text-textPrimary font-semibold text-2xl mx-16 mt-2">
+          Plataforma de recursos para administrar o cuidado e a qualidade de
+          vida de idosos
+        </p>
+        <button className="bg-primary h-12 w-40 mt-5 mx-16 mb-5 rounded text-surface font-semibold">
+          Fazer login
+        </button>
+      </div>
+      <img src="" alt="" />
+    </div>
+  );
+}
