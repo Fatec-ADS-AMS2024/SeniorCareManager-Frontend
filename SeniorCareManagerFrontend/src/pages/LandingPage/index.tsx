7eb951d00b2408b1687578a4de4539d45c83ab4d
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-04 17:02:13 -0300

    :lipstick: feat: Ajustando tamanho container

--- SeniorCareManagerFrontend/src/pages/LandingPage/index.tsx
+++ SeniorCareManagerFrontend/src/pages/LandingPage/index.tsx
@@ -1,16 +1,16 @@
 export default function LandingPage() {
   return (
     <div className="">
-      <div className="w-full bg-surface flex flex-col ">
-        <h1 className="text-secondary font-bold text-4xl mx-16 mt-28">
+      <div className="w-[40%] h-full bg-surface flex flex-col justify-center">
+        <h1 className="text-secondary font-bold text-4xl mx-16">
           Plataforma de ferramentas para gerenciar o cuidado e bem-estar de
           idosos.
         </h1>
-        <p className="text-textPrimary font-semibold text-2xl mx-16 mt-2">
+        <p className="text-textSecondary font-semibold text-2xl mt-2 mx-16">
           Plataforma de recursos para administrar o cuidado e a qualidade de
           vida de idosos
         </p>
-        <button className="bg-primary h-12 w-40 mt-5 mx-16 mb-5 rounded text-surface font-semibold">
+        <button className="bg-primary h-12 w-44 mt-5 rounded text-surface font-semibold hover:bg-secondary transition-colors mx-16 text-xl">
           Fazer login
         </button>
       </div>
