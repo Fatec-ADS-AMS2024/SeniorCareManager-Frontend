a71a46c0b166ab5e672ddb5a0b7e57d514c13141
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-18 13:26:01 -0300

    :sparkles: feat: Criando pasta do componente Card

--- SeniorCareManagerFrontend/src/pages/ReligionRegistration/index.tsx
+++ SeniorCareManagerFrontend/src/pages/ReligionRegistration/index.tsx
@@ -1,7 +1,7 @@
 export default function ReligionRegistration() {
     return (
       <div className="p-4">
-        <h1 className="text-2xl font-bold">Página de Cadastro Religião</h1>
+        <h1 className="text-2xl font-bold">Página de Cadastros</h1>
       </div>
     );
   }
