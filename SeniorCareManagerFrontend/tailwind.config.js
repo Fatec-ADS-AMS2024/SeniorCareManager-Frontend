4edf99b024f80ccc587c576fd08affdcf5325575
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-19 22:37:19 -0300

    :lipstick: feat: Reconfigurando cores do sistema

--- SeniorCareManagerFrontend/tailwind.config.js
+++ SeniorCareManagerFrontend/tailwind.config.js
@@ -15,6 +15,14 @@ export default {
         surface: 'var(--color-surface)',
         surfaceUser: 'var(--color-surface-user)',
         hoverButton: 'var(--color-hoverButton)',
+
+        danger: 'var(--color-danger)',
+        hoverDanger: 'var(--color-hoverDanger)',
+        edit: 'var(--color-edit)',
+        hoverEdit: 'var(--color-hoverEdit)',
+        success: 'var(--color-success)',
+        hoverSuccess: 'var(--color-hoverSuccess)',
+        warning: 'var(--color-warning)',
       }
     },
   },
