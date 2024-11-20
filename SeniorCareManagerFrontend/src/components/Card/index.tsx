09f4464db6bb36097199d565e64ebf9379069080
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-19 23:17:53 -0300

    :sparkles: feat: Subindo novos componentes (Card, GenericModal, SearchBar)

--- SeniorCareManagerFrontend/src/components/Card/index.tsx
+++ SeniorCareManagerFrontend/src/components/Card/index.tsx
@@ -1,5 +1,29 @@
-export default function Card() {
+import { JSX } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface CardProps {
+    text: string;
+    subText: string;
+    icon: JSX.Element;
+    page: string;
+}
+
+export default function Card({ text, subText, icon, page }: CardProps) {
+    const navigate = useNavigate();
+
     return (
-        <div></div>
+        <div className="w-64 h-40 bg-surface rounded-lg shadow-md border border-background cursor-pointer hover:shadow-lg transit" onClick={() => navigate(page)} >
+            <div className="h-1/4 relative ">
+                <div className="flex items-center justify-center w-12 h-12 rounded-full mb-2 text-textSecondary absolute top-3 left-3 bg-background">
+                    {icon}
+                </div>
+            </div>
+            <div className="flex justify-center items-center h-3/4 border-t">
+                <div className="z-10 flex flex-col items-center justify-center text-textPrimary">
+                    <p className="text-lg font-medium">{text}</p>
+                    <p className="text-sm text-textSecondary">{subText}</p> 
+                </div>
+            </div>
+        </div>
     );
 }
--- SeniorCareManagerFrontend/src/pages/Registrations/index.tsx
+++ SeniorCareManagerFrontend/src/pages/Registrations/index.tsx
@@ -1,7 +1,12 @@
+import { FolderPlus } from "@phosphor-icons/react";
+import Card from "../../components/Card";
+import { routes } from "../../routes/routes";
+
 export default function Registrations() {
     return (
       <div className="p-4">
         <h1 className="text-2xl font-bold">Página de Cadastros</h1>
+        <Card text="Religião" subText="Religiões Cadastradas" icon={<FolderPlus/>} page={routes.RELIGIONREGISTRATION}/>
       </div>
     );
   }
