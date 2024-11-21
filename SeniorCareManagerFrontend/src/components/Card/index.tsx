b3150bedac87202a06dcc891878e88ab16e27bb0
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-21 14:30:58 -0300

    :lipstick: feat: Card Estilizado

--- SeniorCareManagerFrontend/src/components/Card/index.tsx
+++ SeniorCareManagerFrontend/src/components/Card/index.tsx
@@ -18,13 +18,13 @@ export default function Card({ text, subText, icon, page }: CardProps) {
     >
       {/* Ícone */}
       <div className="h-1/4 relative">
-        <div className="flex items-center justify-center w-14 h-14 rounded-full mb-2 text-surface p-3 absolute top-3 left-3 bg-[#E0E3E0]">
+        <div className="flex items-center justify-center w-14 h-14 rounded-full mb-2 text-surface p-3 absolute top-3 left-3 bg-[#D9D9D9]">
           {icon}
         </div>
       </div>
 
       {/* Conteúdo */}
-      <div className="flex items-start justify-start mt-5 h-3/4 border-t p-4">
+      <div className="flex items-start justify-start pt-8 mt-1 h-3/4 border-t px-4">
         <div className="z-10 flex flex-col text-textPrimary">
           <p className="text-lg font-medium text-left">{text}</p>
           <p className="text-sm text-left text-textSecondary">{subText}</p>
