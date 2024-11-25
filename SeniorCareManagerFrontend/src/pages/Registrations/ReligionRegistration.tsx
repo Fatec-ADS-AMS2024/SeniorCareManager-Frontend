b166fed9a681d95a61fb6b6079b0121a836be5a5
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-25 16:01:30 -0300

    :lipstick: feat: Configurando estilo das páginas Registration e ReligionRegistration

--- SeniorCareManagerFrontend/src/pages/Registrations/ReligionRegistration.tsx
+++ SeniorCareManagerFrontend/src/pages/Registrations/ReligionRegistration.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react';
 import ReligionService from '../../services/religionService';
 import Religion from '../../types/models/Religion';
 import Table from '../../components/Table';
-import { Pencil, Trash } from '@phosphor-icons/react';
+import { Pencil, Plus, Trash } from '@phosphor-icons/react';
 import BreadcrumbPageTitle from '../../components/BreadcrumbPageTitle';
+import SearchBar from '../../components/SearchBar';
+import Button from '../../components/Button';
 
 export default function ReligionRegistration() {
   const columns = ['Nome'];
@@ -33,10 +35,14 @@ export default function ReligionRegistration() {
 
   return (
     <div>
-      <BreadcrumbPageTitle title="Cadastro de Religião" />
-      <div className="px-4">
-        <Table columns={columns} data={data} actions={<Actions />} />
+    <BreadcrumbPageTitle title="Cadastro de Religião" />
+    <div className="bg-neutralWhite px-6 py-6 max-w-[1400px] mx-auto rounded-lg shadow-md mt-10">
+      <div className="flex items-center justify-between mb-4">
+        <SearchBar />
+        <Button label="Adicionar" icon={<Plus />} iconPosition="left" color="success" size="medium" />
       </div>
+      <Table columns={columns} data={data} actions={<Actions />} />
     </div>
+  </div>
   );
 }
