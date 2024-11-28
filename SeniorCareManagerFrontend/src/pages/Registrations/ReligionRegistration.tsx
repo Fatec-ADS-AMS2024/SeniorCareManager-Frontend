1fa61114f999e60fb3f992cdc19d090de7bda159
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-11-27 23:34:21 -0300

    :sparkles: feat: Primeira modal adicionada

--- SeniorCareManagerFrontend/src/pages/Registrations/ReligionRegistration.tsx
+++ SeniorCareManagerFrontend/src/pages/Registrations/ReligionRegistration.tsx
@@ -1,48 +1,80 @@
-import { useEffect, useState } from 'react';
-import ReligionService from '../../services/religionService';
-import Religion from '../../types/models/Religion';
-import Table from '../../components/Table';
-import { Pencil, Plus, Trash } from '@phosphor-icons/react';
-import BreadcrumbPageTitle from '../../components/BreadcrumbPageTitle';
-import SearchBar from '../../components/SearchBar';
-import Button from '../../components/Button';
+import { useEffect, useState } from "react";
+import ReligionService from "../../services/religionService";
+import Religion from "../../types/models/Religion";
+import Table from "../../components/Table";
+import { Pencil, Plus, Trash } from "@phosphor-icons/react";
+import BreadcrumbPageTitle from "../../components/BreadcrumbPageTitle";
+import SearchBar from "../../components/SearchBar";
+import Button from "../../components/Button";
+import Modal from "../../components/GenericModal";
+
+const inputs = [
+  {
+    label: "Nome religião",
+  },
+];
 
 export default function ReligionRegistration() {
-  const columns = ['Nome'];
+  const columns = ["Nome"];
   const [data, setData] = useState<Religion[]>([]);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   useEffect(() => {
     async function fetchData() {
       const religionService = new ReligionService();
       const res = await religionService.getAll();
-      if (res.code === 200 && res.data)
-        setData([...res.data]);
+      if (res.code === 200 && res.data) setData([...res.data]);
     }
 
+  
+
     fetchData();
-  }, [])
+  }, []);
 
   const Actions = () => (
     <>
       <button className="text-edit hover:text-hoverEdit">
-        <Pencil size={24} weight="fill" />
+        <Pencil className="size-6" weight="fill" />
       </button>
       <button className="text-danger hover:text-hoverDanger">
-        <Trash size={24} weight="fill" />
+        <Trash className="size-6" weight="fill" />
       </button>
     </>
-  )
+  );
 
   return (
     <div>
-    <BreadcrumbPageTitle title="Cadastro de Religião" />
-    <div className="bg-neutralWhite px-6 py-6 max-w-[95%] mx-auto rounded-lg shadow-md mt-10">
-      <div className="flex items-center justify-between mb-4">
-        <SearchBar />
-        <Button label="Adicionar" icon={<Plus />} iconPosition="left" color="success" size="medium" />
+      <BreadcrumbPageTitle title="Cadastro de Religião" />
+      <div className="bg-neutralWhite px-6 py-6 max-w-[95%] mx-auto rounded-lg shadow-md mt-10">
+        <div className="flex items-center justify-between mb-4">
+          <SearchBar />
+          <Button
+            label="Adicionar"
+            icon={<Plus />}
+            iconPosition="left"
+            color="success"
+            size="medium"
+            onClick={openModal}
+          />
+          <Modal
+            title="Religião"
+            inputs={inputs}
+            action={console.log}
+            statusModal={showModal}
+            closeModal={closeModal}
+          />
+        </div>
+        <Table columns={columns} data={data} actions={<Actions />} />
       </div>
-      <Table columns={columns} data={data} actions={<Actions />} />
     </div>
-  </div>
   );
 }
