486c48211853682083beaac041d028308259fec5
Larksel <lemuelgames@outlook.com>
2024-12-05 16:08:21 -0300

    :sparkles: feat: paginação da tabela feita

--- SeniorCareManagerFrontend/src/components/Table/TablePagination.tsx
+++ SeniorCareManagerFrontend/src/components/Table/TablePagination.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 interface TablePaginationProps {
   totalPages: number;
   currentPage: number;
@@ -11,8 +9,6 @@ export default function TablePagination({
   currentPage,
   onPageChange,
 }: TablePaginationProps) {
-  const [showDropdown, setShowDropdown] = useState(false);
-
   const handlePrevious = () => {
     if (currentPage > 1) {
       onPageChange(currentPage - 1);
@@ -25,35 +21,11 @@ export default function TablePagination({
     }
   };
 
-  const handleSelectPage = (page: number) => {
-    onPageChange(page);
-    setShowDropdown(false);
-  };
-
-  const renderDropdown = () => (
-    <div
-      className="absolute top-full mt-2 w-40 bg-white border border-gray-200 rounded shadow-lg z-10"
-      onClick={() => setShowDropdown(false)}
-    >
-      {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
-        <button
-          key={page}
-          className={`w-full px-4 py-2 text-left hover:bg-gray-100 ${
-            page === currentPage ? "bg-gray-200 font-bold" : ""
-          }`}
-          onClick={() => handleSelectPage(page)}
-        >
-          Página {page}
-        </button>
-      ))}
-    </div>
-  );
-
   return (
-    <div className="flex items-center justify-center mt-4">
+    <div className="flex items-center justify-end select-none gap-4 w-[50%]">
       {/* Botão Anterior */}
       <button
-        className="px-4 py-2 border border-gray-300 rounded hover:bg-gray-100"
+        className={`px-4 py-2 border border-neutralDark text-textPrimary rounded hover:bg-hoverButton hover:border-neutralWhite hover:text-neutralWhite transition-colors disabled:invisible ${totalPages === 1 ? 'hidden' : ''}`}
         disabled={currentPage === 1}
         onClick={handlePrevious}
       >
@@ -61,19 +33,13 @@ export default function TablePagination({
       </button>
 
       {/* Dropdown de seleção de página */}
-      <div className="relative mx-4">
-        <button
-          className="px-4 py-2 border border-gray-300 rounded hover:bg-gray-100"
-          onClick={() => setShowDropdown(!showDropdown)}
-        >
-          Página {currentPage} de {totalPages}
-        </button>
-        {showDropdown && renderDropdown()}
-      </div>
+      <span className="px-4 py-2 border border-neutralDark rounded">
+        Página {currentPage} de {totalPages}
+      </span>
 
       {/* Botão Próximo */}
       <button
-        className="px-4 py-2 border border-gray-300 rounded hover:bg-gray-100"
+        className={`px-4 py-2 border border-neutralDark text-textPrimary rounded hover:bg-hoverButton hover:border-neutralWhite hover:text-neutralWhite transition-colors disabled:invisible ${totalPages === 1 ? 'hidden' : ''}`}
         disabled={currentPage === totalPages}
         onClick={handleNext}
       >
