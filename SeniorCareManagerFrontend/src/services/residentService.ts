1c8d5e4c34b7349f76ca413085991869a5792233
Lemuel <94942278+Larksel@users.noreply.github.com>
2025-08-25 15:45:56 -0300

    Revert "Sprint 7 - Implementar validações para classe religion"

--- SeniorCareManagerFrontend/src/services/residentService.ts
+++
@@ -1,8 +0,0 @@
-import Resident from "../types/models/Resident";
-import GenericService from "./genericService";
-
-export default class ResidentService extends GenericService<Resident> {
-    constructor() {
-        super('Resident');
-    }
-}
