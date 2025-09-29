3c9f1907066db4540e1bb1b6ab9077ea17a5d56a
Larksel <lemuelgames@outlook.com>
2025-09-29 17:31:37 -0300

    :sparkles: feat: Função utilitária para criar as rotas da aplicação

--- SeniorCareManagerFrontend/src/types/app/index.ts
+++ SeniorCareManagerFrontend/src/types/app/index.ts
@@ -1,2 +1,11 @@
+import { ReactNode } from "react";
+
 export type Enum = Record<string, number>;
 export type EnumLabels = Record<Enum[keyof Enum], string>;
+
+export interface RouteDefinition {
+  displayName?: string;
+  path?: string;
+  element: ReactNode;
+  index?: boolean;
+}
